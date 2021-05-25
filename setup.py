02be4658844628bb69f0de65d8fe4b0c59db46f6
Vassil Verguilov <Vassil.Verguilov@gmail.com>
2021-05-26 00:58:33 +0300

    Package setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import codecs
+import os.path
+from setuptools import setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    version=get_version("src/pycaenhv/version.py"),
+    package_dir={"": "src"},
+)
---
+++ src/pycaenhv/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.0.1'
+__libversion__ = '6.1' # Latest lib version that the package is working with
