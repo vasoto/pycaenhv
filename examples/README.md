1c5d418a93dce1d408b33c3b25f8283bef37a25a
Vassil Verguilov <Vassil.Verguilov@gmail.com>
2021-05-26 16:43:05 +0300

    Add basic example

---
+++ examples/README.md
@@ -0,0 +1,31 @@
+# Examples
+
+## Prerequisites
+To use the examples please set following environmental variables according to your setup.
+* CAENHV_BOARD_TYPE: The type of the board:
+    SY1527
+    SY2527
+    SY4527
+    SY5527
+    N568
+    V65XX
+    N1470
+    V8100
+    N568E
+    DT55XX
+    FTK
+    DT55XXE
+    N1068
+    SMARTHV
+    NGPS
+* CAENHV_LINK_TYPE: Link to the board:
+    TCPIP
+    RS232
+    CAENET
+    USB
+    OPTLINK
+    USB_VCP
+* CAENHV_BOARD_ADDRESS: Board address
+* CAENHV_USER: (only if set) User name
+* CAENHV_PASSWORD: (only if set) Password
+
---
+++ examples/basic.py
@@ -0,0 +1,39 @@
+import os
+
+from pycaenhv.wrappers import init_system, deinit_system, get_board_parameters, get_crate_map
+from pycaenhv.enums import CAENHV_SYSTEM_TYPE, LinkType
+from pycaenhv.errors import CAENHVError
+
+
+def main():
+    system_type = CAENHV_SYSTEM_TYPE[os.environ['CAENHV_BOARD_TYPE']]
+    link_type = LinkType[os.environ['CAENHV_LINK_TYPE']]
+    handle = init_system(system_type, link_type,
+                         os.environ['CAENHV_BOARD_ADDRESS'],
+                         os.environ.get('CAENHV_USER', ''),
+                         os.environ.get('CAENHV_PASSWORD', ''))
+    try:
+        print(f"Got handle: {handle}")
+        crate_map = get_crate_map(handle)
+        for name, value in crate_map.items():
+            print(name, value)
+        board_parameters = get_board_parameters(handle, 0)
+        print(f"Board parameters: {board_parameters}")
+
+    except CAENHVError as err:
+        print("Got error: {err}\nExiting ...")
+    finally:
+        print("Deinitialize.")
+        deinit_system(handle=handle)
+    print("Bye bye.")
+
+
+if __name__ == '__main__':
+    """ Set environment variables
+    CAENHV_BOARD_TYPE
+    CAENHV_LINK_TYPE
+    CAENHV_BOARD_ADDRESS
+    CAENHV_USER (if set)
+    CAENHV_PASSWORD (if set)
+    """
+    main()
