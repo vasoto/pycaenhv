ce4bae402b43f219d42efad84953d4bcaf025b42
Vassil Verguilov <Vassil.Verguilov@gmail.com>
2021-05-29 15:47:59 +0300

    Fix context to lazy convert values

---
+++ src/pycaenhv/context.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from pycaenhv.wrappers import init_system, deinit_system
+from pycaenhv.enums import CAENHV_SYSTEM_TYPE, LinkType
+
+
+class HVContext:
+    def __init__(self,
+                 system: str,
+                 link: str,
+                 argument: Any,
+                 user: str = '',
+                 password: str = ''):
+        self.system = system
+        self.link = link
+        self.argument = argument
+        self.user = user
+        self.password = password
+        self.handle: int = None
+
+    def __enter__(self):
+        try:
+            system = CAENHV_SYSTEM_TYPE[self.system.upper()]
+            link = LinkType[self.link.upper()]
+        except AttributeError as err:
+            print("Missing value")
+        self.handle = init_system(system, link, self.argument, self.user,
+                                  self.password)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        deinit_system(self.handle)
